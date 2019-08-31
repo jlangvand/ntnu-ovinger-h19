d8de13579bb244b70e63012d5e88285cb81cce76
Joakim Skogø <ajlngvand@gmail.com>
2019-08-31 17:11:11 +0200

    Opprydding

--- CurrencyCalculator/src/no/iskra/ccalculator/CurrencyCalculator.java
+++ CurrencyCalculator/src/no/iskra/ccalculator/CurrencyCalculator.java
@@ -9,6 +9,7 @@ import java.io.DataOutputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.InputMismatchException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.simple.parser.ParseException;
@@ -18,21 +19,42 @@ class CurrencyCalculator {
   static Currency nok;
   static Currency sek;
   static Currency usd;
+  static JSONObject rates;
 
   static Scanner input;
+
+  static boolean exit;
+  static String lastUpdated;
 
   public static void main(String[] args) {
     nok = new Currency("Norske kroner", "NOK", 1);
     usd = new Currency("Amerikanske dollar", "USD", 7.5);
     sek = new Currency("Svenske kronor", "SEK", 0.93);
 
+    System.out.println("Henter valutakurser...\n");
+
+    rates = getCurrencyRates();
+
     input = new Scanner(System.in);
 
-    //rateMenu();
-    getCurrencyRates();
-    System.out.println(String.format("SEK: %.2f\nUSD: %.2f", sek.getRate(), usd.getRate()));
+    exit = false;
+
+    while (!exit) {
+      rateMenu();
+      if (!exit) waitForInput();
+    }
 
     System.out.println();
+  }
+
+  static void waitForInput() {
+    System.out.println();
+    System.out.println("Trykk enter for å gå tilbake...");
+    try {
+      System.in.read();
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
   }
 
   // Usexy metode for å demonstrere bruk av switch
@@ -40,6 +62,7 @@ class CurrencyCalculator {
     printCurrencyMenu();
     double rate = 0;
     String symbol = "";
+    boolean print = true;
     switch (getIntFromUser()) {
       case 1:
         rate = nok.getRate();
@@ -53,15 +76,31 @@ class CurrencyCalculator {
         rate = usd.getRate();
         symbol = usd.getSymbol();
         break;
+      case 4:
+        displayAllCurrencies();
+        print = false;
+        break;
+      case 0:
+        exit = true;
+        print = false;
+        break;
       default:
         break;
     }
     System.out.println();
-    if (rate == 0) {
-      System.out.println("En rar feil har oppstått..");
-    } else {
+    if (rate == 0 && print) {
+      System.out.println("Bare bruk heltall!");
+    } else if (print) {
       System.out.println(String.format("1.00%s = %.2fNOK", symbol, rate));
     }
+  }
+
+  static void displayAllCurrencies() {
+    System.out.println(String.format("Viser verdier i %s", rates.getString("base")));
+    System.out.println(String.format("Sist oppdatert: %s", rates.getString("date")));
+    System.out.println();
+    System.out.println(String.format("SEK: %.2f %s", sek.getRate(), rates.getString("base")));
+    System.out.println(String.format("USD: %.2f %s", usd.getRate(), rates.getString("base")));
   }
 
   static void printCurrencyMenu() {
@@ -69,14 +108,22 @@ class CurrencyCalculator {
     System.out.println("1 - NOK");
     System.out.println("2 - SEK");
     System.out.println("3 - USD");
+    System.out.println("4 - Vis alle");
+    System.out.println("0 - Avslutt");
   }
 
   static int getIntFromUser() {
     System.out.print("\n> ");
-    return input.nextInt();
+    int retVal = 99;
+    try {
+      retVal = input.nextInt();
+    } catch(InputMismatchException e) {
+
+    }
+    return retVal;
   }
 
-  static boolean getCurrencyRates() {
+  static JSONObject getCurrencyRates() {
     JSONObject obj;
 
     try {
@@ -93,14 +140,16 @@ class CurrencyCalculator {
       obj = new JSONObject(str);
       JSONObject currentRates = new JSONObject(obj.getJSONObject("rates"));
 
-      sek.setRate(obj.getJSONObject("rates").getDouble("SEK"));
-      usd.setRate(obj.getJSONObject("rates").getDouble("USD"));
+      sek.setRate(1/obj.getJSONObject("rates").getDouble("SEK"));
+      usd.setRate(1/obj.getJSONObject("rates").getDouble("USD"));
+
+      lastUpdated = obj.getString("date");
     } catch(Exception e) {
       e.printStackTrace();
-      return false;
+      return new JSONObject();
     }
 
-    return true;
+    return obj;
   }
 
   public static String getHTML(String urlToRead) throws Exception {
